06b2137eff3a1085c65907081ad787cb29f8815a
Ojasvi Marwari <ojasvi.0108@gmail.com>
2026-02-02 18:36:57 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,30 @@
+const accId = 1234
+let accEmail = "hello@gmail.com"
+var accCity = "Jaipur"
+let acc=10
+
+console.table([accId, accEmail, accCity, acc])
+
+// accId=9890; //not allowed
+accEmail = "hc.hc@com"
+accCity = "Delhi"
+acc=100
+let accState;
+var accS;
+
+console.table([accId, accEmail, accCity, acc, accState, accS])
+
+/*
+prefer not to use var because of issue in block
+and functional scope
+*/ 
+
+
+// Feature	                           var	                  let	       const
+// Scope	                           Function scope	      Block scope  Block scope
+// Can reassign value?	               ✅ Yes	            ✅ Yes	    ❌ No
+// Can redeclare in same scope?	       ✅ Yes	            ❌ No	    ❌ No
+// Hoisted?	                           ✅ Yes	            ✅ Yes	    ✅ Yes
+// Initialized during hoisting?	       ✅ Yes (undefined)	❌ No (TDZ)	❌ No (TDZ)
+// Block {} respect?	               ❌ No               	✅ Yes	    ✅ Yes
+// Must assign value at declaration?   ❌ No	                ❌ No	    ✅ Yes
